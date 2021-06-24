3520f45097440e1c6c8bd250783297792113baa2
Sagar Dalal <sagardalal112@gmail.com>
2021-06-24 17:01:44 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ mongoose.connection.once("open", () => {
   console.log("Mongodb connection established successfully");
 });
 
-app.get(process.env.PORT + "/", (req, res) => {
+app.get("/", (req, res) => {
   //my code start
   const client = new MongoClient(url, {
     useNewUrlParser: true,
