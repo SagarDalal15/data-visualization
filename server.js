e081b2828689401a57767428c5ccf39f4966abf8
Sagar Dalal <sagardalal112@gmail.com>
2021-06-24 17:22:23 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 //My code start 2
 const url =
